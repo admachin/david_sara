2256e1a3d1fce6243e7487526fc00f0e00b6b631
Adolfo Javier Machín Fernández <adolfomachin@yahoo.es>
2021-09-24 18:36:21 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,31 +1,53 @@
-const map = new Map(
+const ENTRANTS = new Map(
   [
     ["Adolfo", "ValorAdolfo"],
     ["David", "ValorDavid"],
     ["Alberto", "ValorAlberto"],
-    ["Rubén", "ValorRubén"],
+    ["Rubén", "ValorRubén"]
   ]
 );
 
-function getMatch(name) {
-  if(map.has(name)) {
-    return map.get(name);
-  }
-  else {
-    return "Nadie quiere a milhouse :(";
-  }
+const rollEl = document.querySelector(".roll");
+const namesEl = document.querySelector(".names");
+const winnerEl = document.querySelector(".winner");
+
+function randomName() {
+  const rand = Math.floor(Math.random() * ENTRANTS.size);
+  const key = Array.from(ENTRANTS.keys())[rand];
+  const name = ENTRANTS.get(key);
+  namesEl.innerText = name;
 }
 
-function showMatch() {
-  var guest = document.getElementById("guest").value;
-  //output = getMatch(document.getElementById("guest").value);
-  
-  var output = getMatch(guest);
-  
-  console.log('guest: ', guest);
-  
-  document.getElementById('output_1').hidden = false;
-  document.getElementById('output_2').hidden = false;
-  document.getElementById('output_2').innerHTML = output;
+function rollClick() {
+  rollEl.classList.add("hide");
+  winnerEl.classList.add("hide");
+  namesEl.classList.remove("hide");
+
+  setDeceleratingTimeout(randomName, 10, 30);
+
+  setTimeout(() => {
+    namesEl.classList.add("hide");
+    winnerEl.classList.remove("hide");
+
+    const guest = document.getElementById("text-input").value;
+    var winner = "Nadie quiere a Milhouse :(";
+    if(ENTRANTS.has(guest)) {
+      winner = ENTRANTS.get(guest);
+    }
+    winnerEl.innerHTML = `<span>And the winner is...</span><br>${winner}`;
+    rollEl.classList.remove("hide");
+  }, 4000);
 }
 
+function setDeceleratingTimeout(callback, factor, times) {
+  const internalCallback = ((t, counter) => {
+    return () => {
+      if (--t > 0) {
+        setTimeout(internalCallback, ++counter * factor);
+        callback();
+      }
+    };
+  })(times, 0);
+
+  setTimeout(internalCallback, factor);
+}
